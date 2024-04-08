13978b84d23c443689b3960bb0d5d95e4b6ee6c6
Antonio Martinez Brotons <martinezbro@esat-alumni.com>
2024-04-08 17:24:29 +0200

    add is_almost_zero functions for floats and doubles

--- amath_utils.h
+++ amath_utils.h
@@ -1,8 +1,13 @@
 #pragma once
 
+#include <cfloat>
+
 namespace amath {
 constexpr float kSmallNumber = 1.e-8f;
 
 constexpr float PI = 3.14159265359;
 
-}
+bool is_almost_zero(float x, float tolerance = FLT_EPSILON) { return fabsf(x) < tolerance; }
+bool is_almost_zero(double x, double tolerance = DBL_EPSILON) { return fabs(x) < tolerance; }
+
+} // namespace amath
