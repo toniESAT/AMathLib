8410bc25071e3841210456b2105587d7502f2128
Antonio Martinez Brotons <10654467+toni-neurosc@users.noreply.github.com>
2024-09-28 17:40:29 +0200

    restructure directories, add .clang-format

--- include/amath/amath_bit.hpp
+++ include/amath/amath_bit.hpp
@@ -4,5 +4,7 @@
 
 namespace amath {
 // http://graphics.stanford.edu/~seander/bithacks.html#DetermineIfPowerOf2
-bool IsPowerOfTwo(uint64_t x) { return (x != 0) && ((x & (x - 1)) == 0); }
-} // namespace amath
+bool IsPowerOfTwo(uint64_t x) {
+  return (x != 0) && ((x & (x - 1)) == 0);
+}
+}  // namespace amath
