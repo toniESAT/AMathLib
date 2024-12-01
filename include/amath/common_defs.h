73dd18782ffbc2ae06c1649bb86a1c048cd254fe
Antonio Martinez Brotons <10654467+toni-neurosc@users.noreply.github.com>
2024-12-01 16:03:32 +0100

    common_defs.h: remove some unused macros and change integer type macros to namespace scoped typedefs

--- include/amath/common_defs.h
+++ include/amath/common_defs.h
@@ -4,17 +4,14 @@
 #include <float.h>
 #include <stdint.h>
 
-#define u64 uint64_t
-#define u32 uint32_t
-#define u8 uint8_t
-#define s64 int64_t
-#define s32 int32_t
-#define s8 int8_t
-
-#define min(a, b) ((a) < (b) ? (a) : (b))
-#define max(a, b) ((a) > (b) ? (a) : (b))
-
 namespace amath {
+
+typedef uint64_t u64;
+typedef uint32_t u32;
+typedef uint8_t u8;
+typedef int64_t s64;
+typedef int32_t s32;
+typedef int8_t s8;
 
 #ifdef AM_DOUBLE_PRECISON
 typedef double scalar;
@@ -24,8 +21,8 @@ typedef float scalar;
 #define AM_EPSILON FLT_EPSILON
 #endif
 
-const scalar kSmallNumber = 1.e-8f;
-const scalar PI = 3.141592653589793;
+const scalar kSmallNumber = (scalar)1.e-8;
+const scalar PI = (scalar)3.141592653589793;
 
 }  // namespace amath
 
